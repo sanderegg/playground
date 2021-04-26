037a4aeca1795263e2a5992b44407cb679a622d7
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-04-26 11:18:53 +0200

    twisted testing

--- twisted/twisted_test.py
+++ twisted/twisted_test.py
@@ -1,17 +1,46 @@
 from twisted.web import server, resource, static, script
 from twisted.internet import reactor, endpoints
-
+from twisted.web.resource import Resource
+import time
 import test_resource
 
-class Simple(resource.Resource):
+class Hello(Resource):
+    # isLeaf = True
+    # def getChild(self, name, request):
+    #     if name == '':
+    #         return self
+    #     return Resource.getChild(self, name, request)
+    def render_GET(self, request):
+        print(request.uri)
+        return "Hello, world! I am located at {}.".format(request.postpath).encode("utf-8")
+
+class ClockPage(Resource):
     isLeaf = True
     def render_GET(self, request):
-        return "<html>Hello, world!</html>".encode('utf-8')
+        return (b"<!DOCTYPE html><html><head><meta charset='utf-8'>"
+                b"<title></title></head><body>" + time.ctime().encode('utf-8'))
 
-root = static.File("www/", ignoredExts=(".rpy",))
-root.processors = {'.rpy': script.ResourceScript}
-root.putChild('fred', test_resource.Hello())
-root.putChild('bob', test_resource.Hello())
+class Bob(Resource):
+    isLeaf = True
+    def getChild(self, name, request):
+        if name == '':
+            return self
+        return Resource.getChild(self, name, request)
+    def render_GET(self, request):
+        print(request.uri)
+        return "Hello, world! I am Bob {}.".format(request.postpath).encode("utf-8")
+
+
+root = Resource()# static.File("www/", ignoredExts=(".rpy",))
+
+files = static.File("www/", ignoredExts=(".rpy",))
+# files.processors = {'.rpy': script.ResourceScript}
+root.putChild(b"files", files)
+# root.putChild('fred', Hello())
+# root.putChild('bob', Hello())
+
+root.putChild(b'bob', Bob())
+root.putChild(b"clock", ClockPage())
 site = server.Site(root)
 endpoint = endpoints.TCP4ServerEndpoint(reactor, 8080)
 endpoint.listen(site)
