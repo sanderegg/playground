95e4628b5626ca865297be4667b1f02439a37958
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-05-04 10:00:34 +0200

    added docker example dags

--- airflow/dags/docker_swarm_example.py
+++ airflow/dags/docker_swarm_example.py
@@ -15,39 +15,34 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from datetime import timedelta
+
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.providers.docker.operators.docker_swarm import DockerSwarmOperator
 from airflow.utils.dates import days_ago
 
 default_args = {
     "owner": "airflow",
+    "depends_on_past": False,
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
 }
 
 dag = DAG(
-    "docker_sample",
+    "docker_swarm_sample",
     default_args=default_args,
-    start_date=days_ago(2),
+    schedule_interval=timedelta(minutes=10),
+    start_date=days_ago(1),
+    catchup=False,
 )
 
-t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
-
-t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
-
-t3 = DockerOperator(
-    api_version="auto",
-    command="echo 'hello from inside the container!!!'",
-    force_pull=True,
-    image="ubuntu:latest",
-    task_id="docker_op_tester",
-    network_mode="bridge",
-    dag=dag,
-)
-
-
-t4 = BashOperator(task_id="print_hello", bash_command='echo "hello world!!!"', dag=dag)
-
-
-t1 >> t2
-t1 >> t3
-t3 >> t4
+with dag as dag:
+    t1 = DockerSwarmOperator(
+        api_version="auto",
+        command="echo 'hello from inside the the service!!!'",
+        image="ubuntu:latest",
+        auto_remove=True,
+        task_id="sleep_with_swarm",
+        enable_logging=False,  # airflow bug: https://github.com/apache/airflow/issues/13675
+    )
