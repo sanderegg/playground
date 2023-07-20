2ee7dc0145e63beccc1d9c3c5a459cfbf54d1fb4
sanderegg <35365065+sanderegg@users.noreply.github.com>
2023-07-20 14:20:56 +0200

    devel mode runs

--- docker_user_group/services/app/docker/boot.sh
+++ docker_user_group/services/app/docker/boot.sh
@@ -61,17 +61,14 @@ APP_LOG_LEVEL=${API_SERVER_LOGLEVEL:-${LOG_LEVEL:-${LOGLEVEL:-INFO}}}
 SERVER_LOG_LEVEL=$(echo "${APP_LOG_LEVEL}" | tr '[:upper:]' '[:lower:]')
 print_info "Log-level app/server: $APP_LOG_LEVEL/$SERVER_LOG_LEVEL"
 
-if [ "${SC_BOOT_MODE}" = "debug-ptvsd" ]; then
-  reload_dir_packages=$(find /devel/packages -maxdepth 3 -type d -path "*/src/*" ! -path "*.*" -exec echo '--reload-dir {} \' \;)
-
-  exec uvicorn app:app \
+if [ "${SC_BOOT_MODE}" = "development" ]; then
+  print_info $(ls -tlah)
+  exec uvicorn app.app:app \
       --host 0.0.0.0 \
       --reload \
-      $reload_dir_packages
-      --reload-dir . \
-      --log-level \"${SERVER_LOG_LEVEL}\"
+      --log-level ${SERVER_LOG_LEVEL}
 else
-  exec uvicorn app:app \
+  exec uvicorn app.app:app \
     --host 0.0.0.0 \
     --log-level "${SERVER_LOG_LEVEL}"
 fi
