0fe6453c221f9388cd7baa5156235e0751a310f1
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-05-12 11:37:53 +0200

    added cpu limitations

--- aiohttp-server-test/gunicorn/gunicorn.conf.py
+++ aiohttp-server-test/gunicorn/gunicorn.conf.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
 import multiprocessing
+import os
+import sys
 
 bind = "0.0.0.0:8080"
-workers = multiprocessing.cpu_count() * 2 + 1
+cpu_count = multiprocessing.cpu_count()
+# Use CPU_LIMIT env var if set, otherwise use detected CPUs
+cpu_limit = int(os.environ.get("CPU_LIMIT", cpu_count))
+workers = cpu_limit * 2 + 1
+
+# Print CPU and worker information at startup
+print(f"CPUs detected: {cpu_count}", file=sys.stderr)
+print(f"CPU limit set to: {cpu_limit}", file=sys.stderr)
+print(f"Setting {workers} worker processes", file=sys.stderr)
+
 worker_class = "aiohttp.GunicornWebWorker"
 accesslog = "-"
 errorlog = "-"
