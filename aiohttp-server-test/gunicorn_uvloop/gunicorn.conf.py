28ee9651f27392974b6fd8b964c0eb070478c689
sanderegg <35365065+sanderegg@users.noreply.github.com>
2025-05-13 13:43:42 +0200

    add granian

--- aiohttp-server-test/gunicorn_uvloop/gunicorn.conf.py
+++ aiohttp-server-test/gunicorn_uvloop/gunicorn.conf.py
@@ -9,7 +9,7 @@ bind = "0.0.0.0:8080"
 cpu_count = multiprocessing.cpu_count()
 # Use CPU_LIMIT env var if set, otherwise use detected CPUs
 cpu_limit = int(os.environ.get("CPU_LIMIT", cpu_count))
-workers = cpu_limit * 2 + 1
+workers = 1  # cpu_limit * 2 + 1
 
 # Print CPU and worker information at startup
 print(f"CPUs detected: {cpu_count}", file=sys.stderr)
